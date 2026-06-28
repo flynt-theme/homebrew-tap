3f0459c924fbadb08043035cc145019a43b4137e
Kyle Bridgemohansingh <7591831+kylebsingh@users.noreply.github.com>
2026-06-28 12:33:06 -0700

    bump strike to v0.1.1

--- Formula/strike.rb
+++ Formula/strike.rb
@@ -1,24 +1,24 @@
 class Strike < Formula
   desc "Flynt theme generator - builds themes from palette + templates"
   homepage "https://github.com/flynt-theme/strike"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/flynt-theme/strike/releases/download/v0.1.0/strike-darwin-amd64"
-      sha256 "f04b59da28a3f0bc34092b5fd5f2738e726faad7399ca4d652c2efa1ad3f232f"
+      url "https://github.com/flynt-theme/strike/releases/download/v0.1.1/strike-darwin-amd64"
+      sha256 "5ae3fed2d1badaee3d4caa05e6cb449820b00ebd0942f16e58f1ce0d88d3ebd8"
     end
     on_arm do
-      url "https://github.com/flynt-theme/strike/releases/download/v0.1.0/strike-darwin-arm64"
-      sha256 "ddc3ac5d0d8e386a77c82034bc5bf1645c125628f24dfba5ba48a84bc3823292"
+      url "https://github.com/flynt-theme/strike/releases/download/v0.1.1/strike-darwin-arm64"
+      sha256 "3c7f85666c3a9cd31495df35bb7440fc1570204c39340c8fe75e625237fcd9b9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/flynt-theme/strike/releases/download/v0.1.0/strike-linux-amd64"
-      sha256 "53bfaf8a455fda6866764254467706469f0398d3a567cf45e40f8746ea39998a"
+      url "https://github.com/flynt-theme/strike/releases/download/v0.1.1/strike-linux-amd64"
+      sha256 "1200fa4f3f9bd57cfd345c363b322cbfa4855e02d9516c06fbee719d6bfda996"
     end
   end
 
